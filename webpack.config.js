5700932809f1e247121aed644edb9a037e6c9553
Oliver Cule <ogcule@outlook.com>
2017-10-11 09:13:23 +0100

    update

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: __dirname + 'src/index.js'
+};
+modules: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }}
+    }
+    ]
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+  ]},
+  output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname,'dist')
+   }
+}
