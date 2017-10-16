8fdc8b4904e74ee62c39d49b425cc00119211224
ogcule <ogcule@outlook.com>
2017-10-16 09:11:38 +0100

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './index.scss';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Topics from './components/Topics/Topics';
+import App from './components/App/App';
 
-const NotFound = ({ location }) => (
-  <h1>404.. Can not find page <code> { location.pathname }</code>!</h1>
-);
-NotFound.propTypes = {
-  location: PropTypes.shape({
-    pathname: PropTypes.string,
-  }),
-};
-
-NotFound.defaultProps = {
-  location: {
-    pathname: '',
-  },
-};
-
-const RouterExample = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/topics" component={Topics} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </ul>
-    </div>
-
-  </Router>
-);
-ReactDOM.render(<RouterExample />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
